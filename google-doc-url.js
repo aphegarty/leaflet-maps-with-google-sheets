ab22745162ae6e515b78e5dba81d866f2340ff39
Aaron Hegarty <aphegarty@huskers.unl.edu>
2018-09-03 14:11:26 -0500

    Update Google sheet url in code

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1wSZK_KjRI7ynnYTo3fZN8jvYhtTH9zzHkt-_Q41Ce4I/edit#gid=0';
